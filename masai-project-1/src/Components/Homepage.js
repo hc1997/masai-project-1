9b1f46761c4656b0a40b0264fb1a45c817b60972
haren <harenchrungoo@gmail.com>
2019-08-23 15:06:22 +0530

    modified project

--- masai-project-1/src/Components/Homepage.js
+++ masai-project-1/src/Components/Homepage.js
@@ -1,40 +1,35 @@
 import React from 'react';
+// import axios from "axios";
 
 
 class Homepage extends React.Component {
   render() {
-    const authurl = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81nswrg1dgju2q&redirect_uri=https://www.google.com/&scope=r_liteprofile";
-
     return (
      <div>
+       <div className="jumbotron" style={{backgroundColor:"#1C1854"}}>
+       <h1 className="float-right display-3 mt-n5" style={{color:"wheat", fontFamily: "'Cinzel' , serif"}}>The<br></br> Guardian</h1>
 
+         <h2 className="float-left mt-n5 ml-4" style={{color:"yellow", fontSize:"40px", fontWeight:"800"}}>Support The Guardian</h2><br></br>
 
-<nav className="navbar navbar-inverse navbar-static-top example6">
-  <div className="container">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar6">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand " style={{
-  width: "200px", color:'white'}} href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81nswrg1dgju2q&redirect_uri=https://www.google.com/&scope=r_liteprofile">ConnectIN</a>
-    </div>
-    <div id="navbar6" className="navbar-collapse collapse">
-      <ul className="nav navbar-nav navbar-right">
-        <li className="active"><a href={authurl}><img src="signin.png" alt="Sign in"/></a>
-</li>
-        <li><a href="#">Sign Up</a></li>
-        </ul>
-    </div>
-  </div>
-</nav>
-< div className="jumbotron">
-        <h2 className="container" style={{  }}> WELCOME TO YOUR PROFESSIONAL COMMUNITY.....</h2>
-
-</div>
-</div>
+          <p className="ml-4 mt-n3" style={{color:"white"}}>Available for everyone, funded by readers </p>
+          <div className="row ml-4">
+          <a class="nav nav-pills" 
+          href="https://support.theguardian.com/int/contribute?INTCMP=header_support_contribute&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%22header_support_contribute%22%2C%22referrerPageviewId%22%3A%22jznvbadub7luwy4hgs6i%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fwww.theguardian.com%2Finternational%22%7D"
+           style={{backgroundColor:"yellow", borderRadius:"20px",color:"#1C1854",width:"120px",fontWeight:"700"}}>Contribute &#8594;</a>
+            <div className="ml-4"></div>
+           <a class="nav nav-pills " 
+          href="https://support.theguardian.com/int/subscribe?INTCMP=header_support_subscribe&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%22header_support_subscribe%22%2C%22referrerPageviewId%22%3A%22jznvbadub7luwy4hgs6i%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fwww.theguardian.com%2Finternational%22%7D"
+           style={{backgroundColor:"yellow", borderRadius:"20px",color:"#1C1854",width:"120px",fontWeight:"700"}}>Subscribe  &#8594;</a>            
+        
+        </div>  
+        <div className="row mt-5 ml-4">
+        <a class="nav nav-pills" href="" style={{color:"white",width:"120px",fontWeight:"700"}}>News</a>
+        <a class="nav nav-pills" href="" style={{color:"white",width:"120px",fontWeight:"700"}}>Opinion</a>
+        <a class="nav nav-pills" href="" style={{color:"white",width:"120px",fontWeight:"700"}}>Sport</a>
+        <a class="nav nav-pills" href="" style={{color:"white",width:"120px",fontWeight:"700"}}>Culture</a>
+        <a class="nav nav-pills" href="" style={{color:"white",width:"120px",fontWeight:"700"}}>Lifestyle</a>        </div>
+       </div>
+     </div>
     );
   }
 }
