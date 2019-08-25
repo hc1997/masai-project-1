f34f55304cc5233278317363d888b7f8323db176
haren <harenchrungoo@gmail.com>
2019-08-25 17:40:29 +0530

    files added

--- masai-project-1/src/Components/App.js
+++ masai-project-1/src/Components/App.js
@@ -52,18 +52,10 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-				{/* <Homepage />
-                        <Link to="/sports">Sport</Link><br />
-                        <Link to="/culture">Culture</Link><br />
-                        <Link to="/Lifestyle">Lifestyle</Link><br />
-			
-						
-						<Route path = "/sports" exact component = {Sport} />
-						<Route path = "/culture" exact component = {Culture} />
-						<Route path = "/Lifestyle" exact component = {Lifestyle} /> */}
+				
 				<div>
        <div className="jumbotron px-2" style={{backgroundColor:"#1C1854",borderRadius:"40px"}}>
-       <h1 className="float-right display-3 mt-n5" style={{color:"wheat",fontFamily: "'Cinzel' , serif"}}>The<br></br> Guardian</h1>
+       <h1 className="float-right display-3 mt-n5" style={{color:"white",fontFamily: "Marcellus SC, serif",fontSize:"90px",fontWeight:"700"}}>The<br></br> Guardian</h1>
 
          <h2 className="float-left mt-n5 ml-4" style={{color:"yellow", fontSize:"40px", fontWeight:"800"}}>Support The Guardian</h2><br></br>
 
@@ -77,34 +69,20 @@ class App extends React.Component {
           href="https://support.theguardian.com/int/subscribe?INTCMP=header_support_subscribe&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_HEADER%22%2C%22componentId%22%3A%22header_support_subscribe%22%2C%22referrerPageviewId%22%3A%22jznvbadub7luwy4hgs6i%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fwww.theguardian.com%2Finternational%22%7D"
            style={{backgroundColor:"yellow", borderRadius:"20px",color:"#1C1854",width:"120px",fontWeight:"700"}}>Subscribe  &#8594;</a>            
         </div>
-        
-        {/* <input type="text" className="form-control float-right "
-        onChange={this.search} style={{width:"140px"}}
-         placeholder="Search...&#128269;"
-         value={this.state.input_value} name="username"></input>
-         <button type="button" className="float-right mr-2" onClick={this.onSubmit}>Search</button> */}
-
         <div className="row mt-5 ml-4">
-        <Link to="/news" className="nav nav-pills" onClick={this.onSubmit}  style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"140px",fontWeight:"100",fontSize:"30px"}}>News<i className="fa fa-search-plus" style={{fontSize:"24px",color:"red"}}></i></Link>
-        <br></br>
-        <Link to="/sports" className="nav nav-pills" style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"140px",fontWeight:"100",fontSize:"30px"}}>Sport<i className='fas fa-skating' style={{fontSize:"24px",color:"red"}}></i></Link> 
-        <Link to="/culture" className="nav nav-pills"style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"160px",fontWeight:"100",fontSize:"30px"}}>Culture<i className="fa fa-flag" style={{fontSize:"20px", color:"red"}}></i></Link>
-         <Link to="/Lifestyle"className="nav nav-pills" style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"170px",fontWeight:"100",fontSize:"30px"}}>Lifestyle<i className="fas fa-tshirt" style={{fontSize:"20px",color:"red"}} ></i></Link>
-		 <Route path = "/news" exact component = {News} />
-		 <Route path = "/sports" exact component = {Sport} />
-						<Route path = "/culture" exact component = {Culture} />
-						<Route path = "/Lifestyle" exact component = {Lifestyle} />  
-                  
-       
-        </div>
-        
-
-       </div>
-     </div>
-						
-			
-			
-            </div>
+			<Link to="/news" className="nav nav-pills" onClick={this.onSubmit}  style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"140px",fontWeight:"100",fontSize:"30px"}}>News<i className="fa fa-search-plus" style={{fontSize:"24px",color:"red"}}></i></Link>
+			<br></br>
+			<Link to="/sports" className="nav nav-pills" style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"140px",fontWeight:"100",fontSize:"30px"}}>Sport<i className='fas fa-skating' style={{fontSize:"24px",color:"red"}}></i></Link> 
+			<Link to="/culture" className="nav nav-pills"style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"160px",fontWeight:"100",fontSize:"30px"}}>Culture<i className="fa fa-flag" style={{fontSize:"20px", color:"red"}}></i></Link>
+			<Link to="/Lifestyle"className="nav nav-pills" style={{fontFamily: "'Cinzel' , serif",backgroundColor:"#1C1854",color:"white",width:"170px",fontWeight:"100",fontSize:"30px"}}>Lifestyle<i className="fas fa-tshirt" style={{fontSize:"20px",color:"red"}} ></i></Link>
+			<Route path = "/news" exact component = {News} />
+			<Route path = "/sports" exact component = {Sport} />
+			<Route path = "/culture" exact component = {Culture} />
+			<Route path = "/Lifestyle" exact component = {Lifestyle} />
+			</div>
+			</div>
+			</div>
+			</div>
         );
     }
 }
--- masai-project-1/src/Components/sport.js
+++ masai-project-1/src/Components/sport.js
@@ -52,21 +52,21 @@ componentDidMount(){
                     <div className="row">
                       { this.state.output.map((output) =>  {
                             return(
-                            <div className = "col-12 col-xl-6 col-lg-4 my-1 ml-3" 
+                            <div className = "col-12 col-xl-6 col-lg-4 my-1 ml-4" 
                                 key={ output.id}>
-                               <div className="card" style= {{width:"1230px", height:"100%", color:"#1C1854"}}>
+                               <div className="card" style= {{width:"1200px", height:"100%", color:"#1C1854"}}>
                                 <div className="card-body">
-                                    <header className="header" style={{fontSize:"3em",color:"#111",fontWeight:"bold",textAlign:"center",marginTop:"30px",
+                                    <header className="header" style={{fontSize:"3em",fontFamily: "Suez One, cursive",color:"#111",fontWeight:"300",textAlign:"center",marginTop:"30px",
                                         paddingBottom:"15px",textShadow:"-1px 1px 0 white , -2px 2px 0 #111"}}>THE GUARDIAN</header>
                                     <hr className="hr" style={{borderBottom:"4px solid #111",boxShadow:"-1px 1px 0 white ,-2px 2px  0 #111",marginBottom:"50px"}}></hr>
                                     <section className="section" style={{WebkitColumnCount:"4",WebkitColumnRule:"1px solid",WebkitColumnGap:"40px #A1A1A1",
                                         MozColumnCount:"4",MozColumnGap:"40px",MozColumnRule:"1px solid #A1A1A1",columnCount:"4",columnGap:"40px",columnRule:"1px solid #A1A1A1",textAlign:"justify"}}>
-                                            <h2 className="card-title" style={{color:"black",marginTop:"0",textAlign:"left"}}>{output.fields.headline}</h2>
+                                            <h2 className="card-title" style={{color:"black",fontFamily: "Abril Fatface, cursive",marginTop:"0",textAlign:"left"}}>{output.fields.headline}</h2>
                                             <img src ={output.fields.thumbnail} style={{width:"100%"}} alt={output.fields.body}></img>
-                                            {/* <p>Standfirst: {output.fields.standfirst.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p> */}
-                                            <p>Trailtext: {output.fields.trailText.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
-                                            <p>Body:{output.fields.bodyText.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
-                                            <p>Byline:{output.fields.byline.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
+                                            <p style={{fontFamily: "Assistant, sans-serif",fontSize:"17px",color:"black"}}>
+                                            Standfirst: {output.fields.standfirst.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
+                                            <p style={{fontFamily: "Assistant, sans-serif",fontSize:"17px",color:"black"}} >Body:{output.fields.bodyText.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
+                                            <p style={{fontFamily: "Assistant, sans-serif",fontSize:"17px",color:"black"}}>Byline:{output.fields.byline.replace('<p>','').replace('</p>', '').replace('<a>','').replace('</a>', '')}</p>
                                     </section>
                                 </div>
                             </div>
